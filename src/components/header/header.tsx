0e12d1daaddfbaeb96b8888e60e2388151e42890
Anugrah <anugrahnunu858@gmail.com>
2025-03-16 13:18:21 +0700

    update file header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import logo from "../../assets/icons8-pokemon-ball-64.png";
 
 const Header: React.FC = () => {
   return (
-    <div className="flex justify-center fixed top-0 w-full bg-red-600 rounded-b-2xl">
+    <div className="flex justify-center fixed top-0 w-full bg-red-600 rounded-b-2xl z-20">
       <img src={logo} alt="Pokemon Logo" className="py-1" />
     </div>
   );
